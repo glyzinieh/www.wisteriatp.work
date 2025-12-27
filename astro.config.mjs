42a413787a83ff45f76eb15674018a15ff1679f4
glyzinieh <contact@wisteriatp.work>
2025-12-28 01:31:15 +0900

    loadEnvを使用して環境変数を取得するようにastro.config.mjsを更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import { defineConfig } from 'astro/config';
 import remarkBreaks from 'remark-breaks';
 import remarkGfm from 'remark-gfm';
 import remarkNotes from 'remark-notes-plugin';
+import { loadEnv } from 'vite';
 
+const { BLOG_CONTENT_DOMAIN, SITE_URL, BASE_PATH } = loadEnv(process.env.NODE_ENV || 'production', process.cwd(), "");
 
-
-const { SITE_URL, BASE_PATH, BLOG_CONTENT_DOMAIN } = import.meta.env;
-
-// https://astro.build/config
 export default defineConfig({
     site: SITE_URL,
     base: BASE_PATH ? BASE_PATH : '/',
