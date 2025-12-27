0463f5507e5df667c575b6ec4d2bb36f087955a3
glyzinieh <contact@wisteriatp.work>
2025-12-28 01:23:42 +0900

    IMAGE_DOMAINをBLOG_CONTENT_DOMAINに置き換え、BASE_PATHを必須に変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import { defineConfig } from 'astro/config';
 import remarkBreaks from 'remark-breaks';
 import remarkGfm from 'remark-gfm';
 import remarkNotes from 'remark-notes-plugin';
-import { loadEnv } from 'vite';
 
 
 
-const { IMAGE_DOMAIN } = loadEnv(process.env.NODE_ENV || 'production', process.cwd(), "");
-const { SITE_URL, BASE_PATH } = loadEnv(process.env.NODE_ENV || 'production', process.cwd(), "");
+const { SITE_URL, BASE_PATH, BLOG_CONTENT_DOMAIN } = import.meta.env;
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,7 +16,7 @@ export default defineConfig({
 
     image: {
         layout: 'full-width',
-        domains: [IMAGE_DOMAIN],
+        domains: [BLOG_CONTENT_DOMAIN],
     },
 
     markdown: {
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,6 @@
 interface ImportMetaEnv {
     readonly SITE_URL: string;
-    readonly BASE_PATH?: string;
+    readonly BASE_PATH: string;
     readonly BLOG_CONTENT_DOMAIN: string;
 }
 
