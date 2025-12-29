f0dd6d0a06e709832f502e1dac528f8c7d380655
glyzinieh <contact@wisteriatp.work>
2025-12-29 16:18:18 +0900

    BLOG_CONTENT_DOMAINをBLOG_CONTENT_URLに変更し、IMAGE_DOMAINを削除

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,6 @@ interface ImportMetaEnv {
     readonly SITE_URL: string;
     readonly BASE_PATH: string;
     readonly BLOG_CONTENT_URL: string;
-    readonly IMAGE_DOMAIN: string;
 }
 
 interface ImportMeta {
